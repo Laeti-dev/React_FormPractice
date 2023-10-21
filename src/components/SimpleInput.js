aaf4b9df1fc54128ce23b3b3130702596aada3b0
Laeti-dev <laetdi@gmail.com>
2023-10-21 17:16:05 +0200

    set a state for name input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  // Create a state to check keystroke to use on the input
+  const [enteredName, setEnteredName] = useState('')
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+
+    // reset form
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          // bind the states to input
+          value={enteredName}
+          />
       </div>
       <div className="form-actions">
         <button>Submit</button>
