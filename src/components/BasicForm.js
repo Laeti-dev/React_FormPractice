bc283ae77df20372c0ffe28ff7fa7e9615913697
Laeti-dev <laetdi@gmail.com>
2023-11-08 05:30:33 +0100

    367 challenge : starting with firstname input

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
+
 const BasicForm = (props) => {
+  const [firstNameInput, setFirstNameInput] = useState('');
+  const [firstNameIsValid, setFirstNameIsValid] = useState(false);
+
+  const firstNameInputChangeHandler = (event) => {
+    setFirstNameInput(event.target.value);
+    if (firstNameInput.trim() !== ''){
+      setFirstNameIsValid(true)
+      console.log(firstNameInput)
+    };
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    // reset
+    setFirstNameInput('')
+    setFirstNameIsValid(false)
+  }
+
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <div className='control-group'>
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            value={firstNameInput}
+            onChange={firstNameInputChangeHandler}
+            />
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
