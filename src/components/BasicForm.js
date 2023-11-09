a58a4bcf47e6780819c912e641a417344bb3ab76
Laeti-dev <laetdi@gmail.com>
2023-11-09 22:31:03 +0100

    367 challenge : add logic for last name an email inputs

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -4,7 +4,17 @@ const BasicForm = (props) => {
   const [firstNameInput, setFirstNameInput] = useState('');
   const [firstNameIsValid, setFirstNameIsValid] = useState(false);
   const [firstNameInputHasBeenTouch, setFirstNameInputHasBeenTouched] = useState(false);
-  const firstNameInputHasError = !firstNameIsValid || firstNameInputHasBeenTouch;
+  const firstNameInputHasError = !firstNameIsValid && firstNameInputHasBeenTouch;
+
+  const [lastNameInput, setLastNameInput] = useState('');
+  const [lastNameIsValid, setLastNameIsValid] = useState(false);
+  const [lastNameInputHasBeenTouch, setLastNameInputHasBeenTouched] = useState(false);
+  const lastNameInputHasError = !lastNameIsValid && lastNameInputHasBeenTouch;
+
+  const [emailInput, setEmailInput] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [emailInputHasBeenTouch, setEmailInputHasBeenTouched] = useState(false);
+  const emailInputHasError = !emailIsValid && emailInputHasBeenTouch;
 
   const firstNameInputChangeHandler = (event) => {
     setFirstNameInput(event.target.value);
@@ -18,15 +28,49 @@ const BasicForm = (props) => {
     setFirstNameInputHasBeenTouched(true);
   };
 
+  const lastNameInputChangeHandler = (event) => {
+    setLastNameInput(event.target.value);
+    if (lastNameInput.trim() !== ''){
+      setLastNameIsValid(true)
+    };
+  };
+
+  const lastNameInputBlurHandler = (event) => {
+    setLastNameInputHasBeenTouched(true);
+  };
+
+  const emailInputChangeHandler = (event) => {
+    setEmailInput(event.target.value);
+    if (emailInput.trim() !== ''){
+      setEmailIsValid(true)
+    };
+  };
+
+  const emailInputBlurHandler = (event) => {
+    setEmailInputHasBeenTouched(true);
+  };
+
   const submitFormHandler = (event) => {
     event.preventDefault();
     // reset
     setFirstNameInput('');
     setFirstNameIsValid(false);
     setFirstNameInputHasBeenTouched(false);
+
+    setLastNameInput('');
+    setLastNameIsValid(false);
+    setLastNameInputHasBeenTouched(false);
+
+    setEmailInput('');
+    setEmailIsValid(false);
+    setEmailInputHasBeenTouched(false);
   };
 
+
+
   const firstNameClass = firstNameInputHasError ? "form-control invalid" : "form-control";
+  const lastNameClass = lastNameInputHasError ? "form-control invalid" : "form-control";
+  const emailClass = emailInputHasError ? "form-control invalid" : "form-control";
 
   return (
     <form onSubmit={submitFormHandler}>
@@ -40,15 +84,30 @@ const BasicForm = (props) => {
             onChange={firstNameInputChangeHandler}
             onBlur={firstNameInputBlurHandler}
             />
+        {firstNameInputHasError && <p className="error-text">First Name can't be empty</p>}
         </div>
-        <div className='form-control'>
+        <div className={lastNameClass}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            value={lastNameInput}
+            onChange={lastNameInputChangeHandler}
+            onBlur={lastNameInputBlurHandler}
+          />
+          {lastNameInputHasError && <p className="error-text">Last Name can't be empty</p>}
         </div>
       </div>
-      <div className='form-control'>
+      <div className={emailClass}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          value={emailInput}
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+        />
+        {emailInputHasError && <p className="error-text">Email should be valid</p>}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
